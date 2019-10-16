7c525a1cdfd89db5f721fdfe4335945dc59be88b
Achraf <macadelic1030@gmail.com>
2019-10-16 04:14:18 +0200

    Premier jet du shell + default json + chargement des fichiers de configurations.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # taskmaster
+# Fichier de configuration
+- La commande à utiliser pour lancer le programme
+- Le nombre de processus à lancer et laisser tourner
+- Choisir de lancer ce programme au démarrage ou non
+- Choisir si le programme doit toujours être relancé, jamais, ou uniquement lorsqu’il
+s’arrête de manière innatendue
+- Quel code de retour represente une sortie "attendue" du programme
+- Combien de temps le programme doit-il tourner après son démarrage pour que
+l’on considère qu’il s’est "lancé correctement"
+- Combien de fois un redémarrage doit être réalisé avant de s’arrêter
+- Quel signal doit être utilisé pour arrêter (i.e. exit gracefully) le programme
+- Combien de temps d’attente après un graceful stop avant de kill le programme
+- Options pour retirer les stdout/stderr du programme ou pour rediriger vers des
+fichiers
+- Des variables d’environnement a set avant de lancer le programme
+- Un répertoire de travail a set avant de lancer le programme
+- Un umask a set avant de lancer le programme
--- taskmaster.js
+++ taskmaster.js
@@ -1,1 +1,95 @@
-console.log("Arguments node: ", process.argv);
+const readline = require('readline');
+const fs = require('fs');
+const os = require('os');
+const PATH = os.homedir();
+let read = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: true,
+    prompt: "\x1B[31mTaskmaster: \x1B[0m"
+});
+
+let main = {
+    isConfigurationValid: true,
+    programs: [],
+    suffixe: ".tm.json",
+    configDir: PATH + "/taskmaster"
+};
+
+class Program {
+    constructor(object){
+        Object.assign(this, object);
+    }
+    get getVariables (){
+        return Object.keys(this).map(x=>this[x]);
+    }
+}
+
+
+let checkTaskMasterDir = () => {
+    try {
+        fs.accessSync(main.configDir, fs.constants.R_OK | fs.constants.W_OK);
+        console.log("Dossier existant");
+    } catch (error) {
+        if (error === "ENOENT") {
+            console.log("Dossier ${PATH}/taskmaster non-existant on le crée ...");
+            fs.mkdir(main.configDir, (err)=>{
+                if (err) console.log("Création interrompue.");
+                console.log("Dossier créé avec succes...");
+            });
+        } else if (error === "EACCESS") {
+            console.log("Droit insuffisant.");
+            main.isConfigurationValid = false;
+            //Initialiser variable pour empecher les manipulations sur les fichiers de configurations
+        } else {
+            main.isConfigurationValid = false;
+            console.log(error.toString());
+        }
+        console.log("Erreur: " + error.code);
+    }
+};
+
+let loadFile = file => {
+    let obj = JSON.parse(fs.readFileSync(PATH + "/taskmaster/" + file, "UTF-8"));
+    let program = new Program(obj);
+    main.programs.push(program);
+    console.log(program.name + " a été ajouté aux programmes.");
+};
+
+let loadConfiguration = () => {
+    if (!main.isConfigurationValid)  return console.log("Dossier de configuration inexistant.");
+   let files =  fs.readdirSync(main.configDir, "UTF-8");
+   files.filter(x=>x.endsWith(main.suffixe)).forEach(loadFile);
+    console.log("Tous les fichiers de configuration ont été chargés")
+};
+
+checkTaskMasterDir();
+loadConfiguration();
+
+
+
+//
+// fs.readFile("./default.tm.json", "UTF-8", (error, data) => {
+//     if (error) throw error;
+//     let obj = JSON.parse(data);
+//     let program = new Program(obj);
+//     console.log(program.getVariables);
+//     console.log("Chargement du fichier effectué.")
+// });
+
+
+//
+//
+// read.prompt(true);
+// read.on('line', (line) =>{
+//     read.prompt(true);
+// });
+
+
+
+process.on("SIGINT", ()=>{
+    console.log("SIGINT");
+    process.exit(0);
+});
+
+
